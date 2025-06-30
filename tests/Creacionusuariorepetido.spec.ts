6473e69c59e5ad0add0657b95957e9d8ddf1f3fd
Miguel Torres <migueltf2004@gmail.com>
2025-06-30 23:00:34 +0200

    Commit 5

--- tests/Creacionusuariorepetido.spec.ts
+++ tests/Creacionusuariorepetido.spec.ts
@@ -1,8 +1,8 @@
-
 import { test, expect } from '@playwright/test';
 import dotenv from 'dotenv';
-import fs from 'fs'; 
-import path from 'path'; 
+import fs from 'fs';
+import path from 'path';
+
 dotenv.config();
 
 if (!process.env.BASE_URL || !process.env.USER || !process.env.PASSWORD) {
@@ -22,6 +22,18 @@ test.describe('Creación de usuario', () => {
 test('test', async ({ page }) => {
 test.setTimeout(120000); // Establece un tiempo de espera de 120 segundos para la prueba
 // Esto es útil para pruebas que pueden tardar más tiempo en completarse, como la creación de un usuario.
+
+// Si
+const alerta = await page.getByRole('dialog', { name: 'Employee was successfully' });// alerta sera el dialogo que se muestra cuando se crea un usuario correctamente
+  
+// En el caso de que alerta en algun monento se muestre el test fallará y mostrará un mensaje de error codigo abajo
+if (await alerta.isVisible().catch(() => false)) {
+  throw new Error('❌ Error: Se mostró una alerta, el test falla aquí.');
+} // Si la alerta es visible, lanza un error para detener la prueba y mostrar el mensaje de error
+
+
+// Si lo de arriba no se cumple en ningun mometno el test continuara normalmente y caundo muestre por segunda vez el bloque ssn se cumplira
+
     const baseUrl = process.env.BASE_URL!;
 
 
@@ -39,17 +51,18 @@ test.setTimeout(120000); // Establece un tiempo de espera de 120 segundos para l
     const response = await page.goto(`${baseUrl}/CoreApp/People/AddEmployee`); // ver los eventos que se disparan cuando termina de cargar la url?
     expect(response?.status()).toBe(200); 
 
-//
-
 // Esto asegura que se ha cargado correctamente el HTML, Imagenes, CSS, JS, etc. de la página antes de continuar con la prueba
     await page.waitForLoadState('load'); 
 // Pero esto no asegura que peticiones AJAX hayan terminado de cargar, por lo que se debe esperar a que las respuestas de las peticiones AJAX sean 200 OK antes de continuar con la prueba
 
-await Promise.all([
-  page.waitForResponse(res => res.url().includes('GetCountriesList') && res.status() === 200),
-  page.waitForResponse(res => res.url().includes('GetProfilesAsElysiumListObject') && res.status() === 200)
-]);
+//await Promise.all([
+//  page.waitForResponse(res => res.url().includes('GetCountriesList') && res.status() === 200),
+//  page.waitForResponse(res => res.url().includes('GetProfilesAsElysiumListObject') && res.status() === 200)
+//]);
 // Con esto de arriba se asegura que las peticiones AJAX especificas han terminado de cargar antes de continuar con la prueba
+
+
+
 
     //await page.waitForTimeout(2000);
 
@@ -57,8 +70,181 @@ await Promise.all([
 //  response.url().includes('GetCountriesList') && response.status() === 200
 //);
 
-
     await page.waitForTimeout(2000);
+
+  // await page.getByText('Save', { exact: true }).click();
+  await page.getByRole('textbox', { name: 'Last Name*' }).click();
+  await page.getByRole('textbox', { name: 'Last Name*' }).fill(user.lastName); // Poner aqui el apellido del usuario que se va a crear
+  
+    //await page.waitForTimeout(1500); 
+
+  await page.getByRole('textbox', { name: 'First Name*' }).click();
+  await page.getByRole('textbox', { name: 'First Name*' }).fill(user.firstName); // Poner aqui el nombre del usuario que se va a crear
+
+//await page.waitForTimeout(1500); 
+
+  await page.locator('#EmpPostalAddress').click();
+  await page.locator('#EmpPostalAddress').fill(user.postalAddress); // Poner aqui la direccion del usuario que se va a crear
+  
+  await page.waitForTimeout(1500);   
+  
+  await page.locator('#EmpPostalZipCode').click();
+  await page.locator('#EmpPostalZipCode').fill(user.postalZipCode); // Poner aqui el codigo postal del usuario que se va a crear
+  
+  await page.waitForTimeout(1500); 
+  
+  await page.getByRole('textbox', { name: 'Home' }).click();
+  await page.getByRole('textbox', { name: 'Home' }).fill(user.phone); // Poner aqui el telefono del usuario que se va a crear
+  
+  await page.waitForTimeout(1500); 
+  
+  await page.locator('div:nth-child(2) > span > .k-dropdown-wrap > .k-input').first().click();
+  await page.waitForTimeout(1500); 
+  await page.getByRole('option', { name: user.municipality }).click(); // Poner aqui el municipio del usuario que se va a crear
+
+  await page.waitForTimeout(1500); 
+  
+  await page.locator('#EmpAddress').click();
+  await page.locator('#EmpAddress').fill(user.address); // Poner aqui la direccion del usuario que se va a crear
+  
+  await page.waitForTimeout(1500); 
+  
+  await page.locator('#EmpZipCode').click();
+  await page.locator('#EmpZipCode').fill(user.zipCode); // Poner aqui el codigo postal del usuario que se va a crear
+  
+  await page.waitForTimeout(1500); 
+  
+  await page.getByRole('textbox', { name: 'Email Address*' }).click();
+  await page.getByRole('textbox', { name: 'Email Address*' }).fill(user.email); // Poner aqui el email del usuario que se va a crear
+  
+  await page.waitForTimeout(1500); 
+  
+  await page.locator('div:nth-child(5) > span > .k-dropdown-wrap > .k-input').first().click();
+  await page.waitForTimeout(1500); 
+  await page.getByRole('option', { name: user.municipality }).click(); // Poner aqui el municipio del usuario que se va a crear
+
+  await page.waitForTimeout(1500); 
+
+  await page.locator('div:nth-child(6) > div:nth-child(2) > span > .k-dropdown-wrap > .k-input').first().click();
+  await page.waitForTimeout(1500); 
+  // En el caso de que sea hombre poner:
+  await page.getByRole('option', { name: 'Male', exact: true }).click();
+  // En el caso de que sea mujer poner:
+  //await page.getByRole('option', { name: 'Female' }).click();
+  
+  //await page.waitForTimeout(1500); 
+    
+  await page.getByRole('combobox', { name: 'Birth Date*' }).click();
+  await page.getByRole('combobox', { name: 'Birth Date*' }).fill(user.birthDate); // Poner aqui la fecha de nacimiento del usuario que se va a crear
+  
+  //await page.waitForTimeout(2000); 
+  
+  await page.getByRole('textbox', { name: 'Social Security*' }).click();
+  await page.getByRole('textbox', { name: 'Social Security*' }).fill(user.ssn); 
+  // Poner aqui el numero de seguro social del usuario que se va a crear en este caso sera repetido para provocar el error en la pagina
+  // 
+  
+    
+  await page.locator('.form-group > div:nth-child(3) > span > .k-dropdown-wrap > .k-input').first().click();
+  await page.waitForSelector(`role=option[name="${user.civilStatus}"]`, { state: 'visible', timeout: 10000 });
+
+  await page.getByRole('option', { name: user.civilStatus }).click(); // Poner el estado civil en name, entre: 'Single', 'Married', 'Widowed', 'Not Specific'
+ // await page.waitForTimeout(1500); 
+  await page.getByRole('link', { name: 'PERSONAL INFORMATION ' }).click();
+
+await page.waitForTimeout(2000); // Esperar que el menu de arriba se cierre para abir el de abajo
+
+  await page.getByRole('link', { name: 'SETTINGS ' }).click();
+  //await page.waitForTimeout(1500); 
+  await page.getByRole('combobox', { name: 'Hire Date*' }).click();
+  await page.getByRole('combobox', { name: 'Hire Date*' }).fill(user.hireDate); // Poner aqui la fecha de contratacion del usuario que se va a crear
+  
+  //await page.waitForTimeout(1500); 
+  
+  await page.locator('div:nth-child(4) > div:nth-child(2) > span > .k-dropdown-wrap > .k-input').first().click();
+  //await page.waitForTimeout(1500); 
+  await page.waitForSelector(`role=option[name="${user.contractType}"]`, { state: 'visible' });
+
+  await page.getByRole('option', { name: user.contractType }).click(); // Poner el tipo de contrato correspondiente.
+
+  //await page.waitForTimeout(1500); 
+  
+  await page.locator('div:nth-child(6) > .col-md-8 > span > .k-dropdown-wrap > .k-input').first().click();
+  //
+  // await page.waitForTimeout(1500); 
+
+  await page.waitForSelector(`role=option[name="${user.payrollGroup}"]`, { state: 'visible'});
+
+  await page.getByRole('option', { name: user.payrollGroup }).click(); // Poner el grupo de nomina correspondiente.
+  
+  //await page.waitForTimeout(1500); 
+
+  await page.getByRole('listbox', { name: 'Payment Type*' }).locator('span').nth(1).click();
+  //
+  // await page.waitForTimeout(1500); 
+  await page.waitForSelector(`role=option[name="${user.paymentType}"]`, { state: 'visible', timeout: 10000 });
+
+  await page.getByRole('option', { name: user.paymentType }).click(); // Poner el tipo de pago correspondiente.
+  
+  //await page.waitForTimeout(1500); 
+
+  await page.getByRole('listbox', { name: 'Profile*' }).locator('span').nth(1).click();
+  await page.waitForSelector(`role=option[name="${user.profile}"]`, { state: 'visible' });
+
+  // await page.waitForTimeout(1500); 
+  await page.getByRole('option', { name: user.profile }).click(); // Poner el perfil correspondiente.
+
+
+  await page.getByRole('textbox', { name: 'Badge Number*' }).click();
+  await page.getByRole('textbox', { name: 'Badge Number*' }).fill(user.badgeNumber); // Poner el numero de placa del usuario, en este caso sera repetido para provocar el error en la pagina
+  // Poner el numero de placa del usuario, en este caso sera repetido para provocar el error en la pagina  
+
+
+  await page.getByRole('textbox', { name: 'Identification No.*' }).click();
+  await page.getByRole('textbox', { name: 'Identification No.*' }).fill(user.identificationNumber); // Poner el numero de identificacion del usuario, en este caso sera repetido para provocar el error en la pagina
+// Poner el numero de identificacion del usuario, en este caso sera repetido para provocar el error en la pagina
+
+  await page.getByRole('textbox', { name: 'Username*' }).click();
+  await page.getByRole('textbox', { name: 'Username*' }).fill(user.userName); // Poner el nombre de usuario del usuario, en este caso sera repetido para provocar el error en la pagina
+// Poner el nombre de usuario del usuario, en este caso sera repetido para provocar el error en la pagina
+  
+  //await page.waitForTimeout(1500); 
+    
+  await page.locator('div:nth-child(3) > div:nth-child(2) > .col-md-8 > span > .k-dropdown-wrap > .k-input').click();
+//  await page.waitForTimeout(1500); 
+  await page.waitForSelector(`role=option[name="${user.payRule}"]`, { state: 'visible'});
+  await page.getByRole('option', { name: user.payRule }).click(); // Regla de pago correspondiente.
+
+// await page.waitForTimeout(1500); 
+
+  await page.getByRole('listbox', { name: 'Cost Center' }).locator('span').nth(1).click();
+  //await page.waitForTimeout(1500);
+await page.waitForSelector(`role=option[name="${user.costCenter}"]`, { state: 'visible'});
+
+  await page.getByRole('option', { name: user.costCenter }).click(); //Selecciona el centro de costo correspondiente.
+  
+  await page.waitForTimeout(1000); 
+  
+  await page.getByRole('link', { name: 'SETTINGS ' }).click();
+  //await page.getByRole('link', { name: 'CUSTOM FIELDS ' }).click();
+  //await page.getByRole('link', { name: 'BANK INFORMATION ' }).click();
+  await page.waitForTimeout(1000); 
+  await page.getByText('Save', { exact: true }).click();
+  
+  await page.waitForTimeout(3000); // Espera 3 segundos para que se guarde el usuario y salga el mensaje de exito
+  
+  await page.getByRole('button', { name: ' Go to profile' }).click();
+await page.waitForTimeout(3000); // Esperar 3 segundos para que se cargue el perfil del usuario creado 
+
+  // await page.getByRole('dialog', { name: 'Alert' }).locator('div').nth(2).click();  //  Esto es del cuadro de dialogo que se muestra cuando se crea un usuario correctamente
+
+
+
+
+// Volvemos a intentar crear el usuario con los mismos datos para provocar el error de usuario repetido
+// El siguientre usuario dara error por tener el mismo SSN, numero de placa y numero de identificacion que el usuario creado anteriormente
+
+
 
     await page.getByRole('textbox', { name: 'Last Name*' }).click();
   await page.getByRole('textbox', { name: 'Last Name*' }).fill(user.lastName); // Poner aqui el apellido del usuario que se va a crear
@@ -216,13 +402,31 @@ await page.waitForSelector(`role=option[name="${user.costCenter}"]`, { state: 'v
   //await page.getByRole('link', { name: 'BANK INFORMATION ' }).click();
   await page.waitForTimeout(1000); 
   await page.getByText('Save', { exact: true }).click();
-    await page.waitForTimeout(3000); // Espera 3 segundos para que se guarde el usuario y salga el mensaje de exito
-  await page.getByRole('button', { name: ' Go to profile' }).click();
+
+
+  // Espera a que aparezca el mensaje de alerta y luego cierra el diálogo, este mensaje mostrara que no se puede crear un usuario con el mismo ssn, username, id y badge
+  await page.getByRole('link', { name: ' OK' }).click();
+
+
+// Si el boton de arriba de presiona correctamente el test se considera exitoso, ya que se ha comprobado que no se puede crear un usuario con los mismos datos
+// Si el boton de arriba no se presiona el test fallara y mostrara un mensaje
 
 
 
+
+  // Finalizar el test
+
+  
 
 });
 });
 
+
+
+
+
+//Hacer que el test se realize una vez , para que no se creen usuarios repetidos.
+//Y que el test sea exitoso si no se crea 
+//
+//ssn, username , id y badge
 
--- tests/pruebaurls.spec.ts
+++ tests/pruebaurls.spec.ts
@@ -1,16 +1,26 @@
 import { test, expect } from '@playwright/test';
 import dotenv from 'dotenv';
-
+import fs from 'fs'; 
+import path from 'path'; 
 dotenv.config();
 
-if (!process.env.BASE_URL || !process.env.USERNAME || !process.env.PASSWORD) {
+
+if (!process.env.BASE_URL || !process.env.USER || !process.env.PASSWORD) {
   throw new Error('Faltan variables de entorno: BASE_URL, USERNAME o PASSWORD');
 }
 
 test.use({ storageState: 'storageState.json' });
 // Utiliza el estado de almacenamiento guardado en 'storageState.json' para mantener la sesión iniciada
 
+
+// Carga las URLs desde un archivo JSON
+const rawData = fs.readFileSync(path.join(__dirname, '../data/urls.json'), 'utf8'); // Lee el archivo JSON que contiene las URLs
+const {urls} = JSON.parse(rawData);  // la variable user contendrá las URLs que se van a visitar en el test
+// Este archivo tiene todas las URLs que se van a visitar en el test
+
 test('Verificacion de todas las URLs ', async ({ page }) => {
+ test.setTimeout(120000); // Establece un tiempo de espera de 120 segundos para la prueba
+
   // Login
 
  const baseUrl = process.env.BASE_URL!; 
@@ -20,41 +30,16 @@ test('Verificacion de todas las URLs ', async ({ page }) => {
 
   await page.waitForTimeout(2000); // Deja tiempo a que cargue el sistema
 
-  // Y ahora uno por uno
-  const urls = [
-    'https://elysium.intechsol-pr.net/CentralApp/DashboardItems/Index/972',
-    'https://elysium.intechsol-pr.net/CoreApp/Dashboard/Index/342',
-    'https://elysium.intechsol-pr.net/CentralApp/Dashboard/Index/971',
-    'https://elysium.intechsol-pr.net/MyEss/Home/Index/955',
-    'https://elysium.intechsol-pr.net/CoreApp/Evaluations/Index/1254',
-    'https://elysium.intechsol-pr.net/CoreApp/ExtPositions/Index/964',
-    'https://elysium.intechsol-pr.net/CoreApp/GlobalEditions/Index/973',
-    'http://www.intechsol-pr.com/help',
-    'https://elysium.intechsol-pr.net/ICS/TaxesForms/Index/914',
-    'https://elysium.intechsol-pr.net/CoreApp/GlobalEditions/ImportExport/1127',
-    'https://elysium.intechsol-pr.net/CentralApp/Menus/Index/916',
-    'https://elysium.intechsol-pr.net/CentralApp/News/Index/360',
-    'https://elysium.intechsol-pr.net/CentralApp/Notifications/Index/1245',
-    'https://elysium.intechsol-pr.net/CoreApp/Payroll/Index/346',
-    'https://elysium.intechsol-pr.net/CentralApp/UserProfile/Index/930',
-    'https://elysium.intechsol-pr.net/CentralApp/Purge/Index/1363',
-    'https://elysium.intechsol-pr.net/CoreApp/Positions/Index/354',
-    'https://elysium.intechsol-pr.net/CoreApp/Reports/Index/356',
-    'https://elysium.intechsol-pr.net/CoreApp/People/Index/917',
-    'https://elysium.intechsol-pr.net/CoreApp/SalaryDifferentials/Setup/1087',
-    'https://elysium.intechsol-pr.net/CoreApp/TAndA/Scheduler/345',
-    'https://elysium.intechsol-pr.net/CentralApp/ServicesManager/Index/1364',
-    'https://elysium.intechsol-pr.net/CoreApp/Setup/Index/358',
-    'https://elysium.intechsol-pr.net/ICS/TaxesForms/Index/1346',
-    'https://elysium.intechsol-pr.net/CoreApp/TAndA/Index/344',
-    'https://elysium.intechsol-pr.net/CentralApp/Users/Index/932',
-    'https://elysium.intechsol-pr.net/CentralApp/Integrations/index/1408'
-  ];
+//const errores: string[] = []; // Array para almacenar errores de estado de las URLs
 
-  for (const url of urls) {
+  for (const url of urls) { // Itera sobre cada URL en el archivo JSON
     const response = await page.goto(url);
-    console.log(`Visitando: ${url}`);
-    expect(response?.status()).toBe(200);
-    await page.waitForTimeout(500); // Pequeña pausa opcional entre páginas
+    const status = response?.status();
+        console.log(`Visitando: ${url} → Estado: ${status}`);
+
+    //if (status !== 200) {
+    //  errores.push(` ${url} → Estado: ${status}`); // esto era para guardar los errores en un array
+    //}
+    await page.waitForTimeout(500); // Pausa entre pagina y pagina 
   }
 });
